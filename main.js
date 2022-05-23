84e8fccd879270bb93540f39e3f964b95ac6fbb1
1RISHAV234 <83695942+1RISHAV234@users.noreply.github.com>
2022-05-23 22:32:37 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function lightmode() {
+    document.getElementById("lol").style.backgroundColor="white";
+    document.getElementById("heading").style.color="black";
+    document.getElementById("light").style.visibility="hidden";
+    document.getElementById("dark").style.visibility="visible"
+}
+function hovered() {
+    document.getElementById("a_link").style.color="gray";
+}
+function darkmode() {
+    document.getElementById("lol").style.backgroundColor="#222";
+    document.getElementById("heading").style.color="white";
+    document.getElementById("dark").style.visibility="hidden";
+    document.getElementById("light").style.visibility="visible";
+}
+//background-color:#222;
